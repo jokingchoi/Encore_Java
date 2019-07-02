732a31fb7f9d2a0521f3f91a76c4f30b96aea957
happymwkang <52415890+happymwkang@users.noreply.github.com>
2019-07-02 17:47:16 +0900

    test
    test

---
+++ Test.java
@@ -0,0 +1,15 @@
+//컴파일 실행 명령어 습득하기
+/* 
+1. 한글 데이터 단순 출력
+2. 컴파일 명령어
+	소스 내장 경로>javac file명.java
+3. 실행 명령어
+	실행파일 내장 경로>java file명
+*/
+
+class Test {
+	public static void main(String [] args) {
+		System.out.println("한글");
+	}
+	
+}
